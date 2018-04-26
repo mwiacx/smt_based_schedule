f8bf9c5380c9d88a4dfa96cd466230cdf3f6464e
mwiacx <759046501@qq.com>
2018-04-26 16:09:13 +0800

    update

--- constraint_generation.py
+++ constraint_generation.py
@@ -1,13 +1,17 @@
+from pysmt.shortcuts import Symbol, Or, GE, LT, Int, LE
+from pysmt.shortcuts import Plus, Times, Minus, Div
+from pysmt.shortcuts import Solver, is_sat
+from pysmt.typing import INT
+
 import math
 import pdb
 import time
-from z3 import *
 
 
-def frame_constraints(slover, frameSet):
-    '''
+def frame_constraints(solver, frameSet):
+    '''0000000000000000
     向求解器中添加帧约束
-        slover: z3求解器实例
+        solver: z3求解器实例
         frameSet: 帧集合
     已验证:
         数值上，消息帧的大小会不会设置的不合理？
@@ -21,9 +25,9 @@ def frame_constraints(slover, frameSet):
             for frame in framelist:
                 aCon = (frame.offset > 0, frame.offset < frame.T - frame.L)
                 # print(aCon)
-                slover.add(aCon)
+                solver.add(aCon)
 
-    # print(slover)
+    # print(solver)
     # pdb.set_trace()
     return True
 
@@ -46,7 +50,7 @@ def lcm(ipt_a, ipt_b):
     return result
 
 
-def link_constraints(slover, frameSetSortedByLink):
+def link_constraints(solver, frameSetSortedByLink):
     '''
     已验证
     '''
@@ -79,20 +83,20 @@ def link_constraints(slover, frameSetSortedByLink):
                             (frame_j.offset + beta * frame_j.T >= frame_i.offset + alpha * frame_i.T + frame_i.L))
                         # print('alpha={},beta={}:\nconstraint={}'.format(
                         #    alpha, beta, single_link_constraint))
-                        slover.add(single_link_constraint)
-                # print(slover)
-    # print(slover.check())
-    # print(slover.model())
+                        solver.add(single_link_constraint)
+                # print(solver)
+    # print(solver.check())
+    # print(solver.model())
     # pdb.set_trace()
     # FIXME:提高效率？
     return True
 
 
-def virtual_link_constraints(slover, frameSet, vlinkSet, g):
+def virtual_link_constraints(solver, frameSet, vlinkSet, g):
     '''
     生产者虚帧先于网络链路帧先于消费者的虚帧
     参数：
-        slover：求解器实例
+        solver：求解器实例
         frameSet: 帧集合(vlid_t -> link -> [frame],两层字典)
         vlinkSet: 包含虚链路信息的集合
         g: 模型中的时间同步精度，单位：us
@@ -113,14 +117,15 @@ def virtual_link_constraints(slover, frameSet, vlinkSet, g):
             frameLastLinkI = frameListLinkI[len(frameListLinkI)-1]
             # 第一个帧
             frameFirstLinkJ = frameListLinkJ[0]
-            aCon = (linkJ.macrotick * frameFirstLinkJ.offset - linkI.delay -
-                    g >= linkI.macrotick * (frameLastLinkI.offset + frameLastLinkI.L))
-            # print(aCon)
-            slover.add(aCon)
-        # print(slover)
+            #aCon = (linkJ.macrotick * frameFirstLinkJ.offset - linkI.delay -
+            #        g >= linkI.macrotick * (frameLastLinkI.offset + frameLastLinkI.L))
+            aCon = GE()
+            print(aCon)
+            solver.add(aCon)
+        # print(solver)
     #time_start = time.time()
-    # print(slover.check())
-    # print(slover.model())
+    # print(solver.check())
+    # print(solver.model())
     #time_end = time.time()
     # print('#计算用时：{} s'.format(time_end-time_start))
     # pdb.set_trace()
@@ -128,11 +133,11 @@ def virtual_link_constraints(slover, frameSet, vlinkSet, g):
     return True
 
 
-def end_to_end_latency_constraints(slover, frameSet, vlinkSet):
+def end_to_end_latency_constraints(solver, frameSet, vlinkSet):
     '''
     端到端延迟不能超过允许的最大延迟，即小于等于tt-message的周期。
     参数：
-        slover:
+        solver:
         frameSet:
     '''
     # 首先选择一条虚链路
@@ -146,14 +151,18 @@ def end_to_end_latency_constraints(slover, frameSet, vlinkSet):
         firstFrame = frameSameVLink[firstLink][0]
         lastFrame = frameSameVLink[lastLink][len(frameSameVLink[lastLink])-1]
         # 约束
-        aCon = (lastLink.macrotick * (lastFrame.offset + lastFrame.L)
-                <= (firstLink.macrotick * firstFrame.offset + vlinkSet[vlid_t].max_latency))
-        # print(aCon)
-        slover.add(aCon)
-    # print(slover)
+        # aCon = (lastLink.macrotick * (lastFrame.offset + lastFrame.L)
+        #        <= (firstLink.macrotick * firstFrame.offset + vlinkSet[vlid_t].max_latency))
+        aCon = LE(Minus(Int(lastLink.macrotick), Plus(
+            lastFrame.offset, Int(lastFrame.L))), 
+            Plus(Minus(Int(firstLink.macrotick), firstFrame.offset), 
+            Int(vlinkSet[vlid_t].max_latency)))
+        print(aCon)
+        solver.add(aCon)
+    # print(solver)
     #time_start = time.time()
-    # print(slover.check())
-    # print(slover.model())
+    # print(solver.check())
+    # print(solver.model())
     #time_end = time.time()
     # print('#计算用时：{} s'.format(time_end-time_start))
     # pdb.set_trace()
@@ -161,11 +170,11 @@ def end_to_end_latency_constraints(slover, frameSet, vlinkSet):
     return True
 
 
-def task_constraints(slover, frameSet, vlinkSet):
+def task_constraints(solver, frameSet, vlinkSet):
     '''
     虚帧顺序执行（由其他约束规定），所有虚帧都处于调度窗口内（offset ---- deadline）
     参数：
-        slover:
+        solver:
         frameSet:
     疑问：这里的task.offset是什么？0？还是设置值？
     '''
@@ -180,12 +189,15 @@ def task_constraints(slover, frameSet, vlinkSet):
         # for frame in frameList:
         #    aCon = (frame.offset >= task.offset)
         #    print(aCon)
-        #    slover.add(aCon)
+        #    solver.add(aCon)
         lastFrame = frameList[len(frameList)-1]
         # FIXME: 这里lastFrame.L与论文理解不同
-        aCon = (lastFrame.offset <= task.D /
-                firstSelfLink.macrotick - lastFrame.L)
-        slover.add(aCon)
+        # aCon = (lastFrame.offset <= task.D /
+        #        firstSelfLink.macrotick - lastFrame.L)
+        aCon = LE(lastFrame.offset, Int(
+            task.D / firstSelfLink.macrotick - lastFrame.L))
+        print(aCon)
+        solver.add(aCon)
         # 如何虚链路不是SelfLink，有消费者
         if len(vl) > 2:
             lastSelfLink = vl[len(vl)-1]
@@ -193,26 +205,29 @@ def task_constraints(slover, frameSet, vlinkSet):
             frameList = frameSameVLink[lastSelfLink]
             # for frame in frameList:
             #    aCon = (frame.offset >= task.offset)
-            #    slover.add(aCon)
+            #    solver.add(aCon)
             lastFrame = frameList[len(frameList)-1]
             # FIXME: 这里lastFrame.L与论文理解不同
-            aCon = (lastFrame.offset <= task.D /
-                    lastSelfLink.macrotick - lastFrame.L)
-            slover.add(aCon)
-    # print(slover)
-    # print(slover.check())
-    # print(slover.model())
+            # aCon = (lastFrame.offset <= task.D /
+            #        lastSelfLink.macrotick - lastFrame.L)
+            aCon = LE(lastFrame.offset, Int(
+                task.D / firstSelfLink.macrotick - lastFrame.L))
+            print(aCon)
+            solver.add(aCon)
+    # print(solver)
+    # print(solver.check())
+    # print(solver.model())
     # pdb.set_trace()
 
     return True
 
 
-def virtual_frame_sequence_constraints(slover, frameSet, vlinkSet):
+def virtual_frame_sequence_constraints(solver, frameSet, vlinkSet):
     '''
     链路约束规定了不同任务的虚帧不会重叠，同样同一个任务的不同虚帧也不同重叠
     注意不同的是，同一节点上不会有精度问题
     参数：
-        slover:
+        solver:
         frameSet:
     '''
     # 首先选择一个条虚链路
@@ -226,9 +241,10 @@ def virtual_frame_sequence_constraints(slover, frameSet, vlinkSet):
         for frameId in range(len(frameList)-1):
             frameI = frameList[frameId]
             frameJ = frameList[frameId+1]
-            aCon = (frameJ.offset >= frameI.offset + frameI.L)
-            # print(aCon)
-            slover.add(aCon)
+            #aCon = (frameJ.offset >= frameI.offset + frameI.L)
+            aCon = GE(frameJ.offset, Plus(frameI.offset, Int(frameI.L)))
+            print(aCon)
+            solver.add(aCon)
 
         # 2.如果虚链路不是SelfLink，有消费者
         if len(vl) > 2:
@@ -238,15 +254,16 @@ def virtual_frame_sequence_constraints(slover, frameSet, vlinkSet):
             for frameId in range(len(frameList)-1):
                 frameI = frameList[frameId]
                 frameJ = frameList[frameId+1]
-                aCon = (frameJ.offset >= frameI.offset + frameI.L)
-                # print(aCon)
-                slover.add(aCon)
-    # print(slover)
+                #aCon = (frameJ.offset >= frameI.offset + frameI.L)
+                aCon = GE(frameJ.offset, Plus(frameI.offset, frameI.L))
+                print(aCon)
+                solver.add(aCon)
+    # print(solver)
     # pdb.set_trace()
     return True
 
 
-def task_precedence_contraints(slover, frameSetSortByTask, taskA, taskB):
+def task_precedence_contraints(solver, frameSetSortByTask, taskA, taskB):
     '''
     任务A优先于任务B：任务A必须先于任务B完成。
     '''
@@ -256,10 +273,12 @@ def task_precedence_contraints(slover, frameSetSortByTask, taskA, taskB):
     fisrtFrameOfB = frameSetB[0]
     linkA = taskA.selfLink
     linkB = taskB.selfLink
-    aCon = linkB.macrotick * fisrtFrameOfB.offset >= linkA.macrotick * \
-        (lastFrameOfA.offset + lastFrameOfA.L)
+    # aCon = linkB.macrotick * fisrtFrameOfB.offset >= linkA.macrotick * \
+    #    (lastFrameOfA.offset + lastFrameOfA.L)
+    aCon = GE(Times(Int(linkB.macrotick), fisrtFrameOfB.offset),
+              Times(Int(linkA.macrotick), Plus(lastFrameOfA.offset, Int(lastFrameOfA.L))))
     print(aCon)
-    slover.add(aCon)
+    solver.add_assertion(aCon)
     return True
 
 
@@ -270,71 +289,72 @@ def define_var(testSet):
         for link in vl_frame_list:
             framelist = vl_frame_list[link]
             for frame in framelist:
-                frame.offset = Int('offset_{}_({})_{}'.format(
-                    frame.vlid, frame.lname[0]+'_'+frame.lname[1], frame.fid))
+                frame.offset = Symbol('offset_{}_({})_{}'.format(
+                    frame.vlid, frame.lname[0]+'_'+frame.lname[1], frame.fid), INT)
                 # print(frame.offset)
     return
 
 
-def z3_run(testSet):
+def smt_run(testSet, smtName):
     if not testSet:
         return False
     # 生成Z3相关的定义
     define_var(testSet)
     # 求解器定义
-    s = Solver()
-    # 生成Frame约束
-    print('\t# 生成frame constraints...')
-    st = time.clock()
-    frame_constraints(s, testSet.frameSet)
-    et = time.clock()
-    print('\t  耗时：{} s'.format(et-st))
-    #
-    print('\t# 生成link constraints...')
-    st = time.clock()
-    link_constraints(s, testSet.frameSetSortByLink)
-    et = time.clock()
-    print('\t  耗时：{} s'.format(et-st))
-    # 同步精度通常为1us
-    print('\t# 生成virtual link constraints...')
-    st = time.clock()
-    virtual_link_constraints(s, testSet.frameSet, testSet.vlinkSet, 1)
-    et = time.clock()
-    print('\t  耗时：{} s'.format(et-st))
-    #
-    print('\t# 生成end to end latency constraints...')
-    st = time.clock()
-    end_to_end_latency_constraints(s, testSet.frameSet, testSet.vlinkSet)
-    et = time.clock()
-    print('\t  耗时：{} s'.format(et-st))
-    #
-    print('\t# 生成task constraints...')
-    st = time.clock()
-    task_constraints(s, testSet.frameSet, testSet.vlinkSet)
-    et = time.clock()
-    print('\t  耗时：{} s'.format(et-st))
-    #
-    print('\t# 生成virtual frame sequence constraints...')
-    st = time.clock()
-    virtual_frame_sequence_constraints(s, testSet.frameSet, testSet.vlinkSet)
-    et = time.clock()
-    print('\t  耗时：{} s'.format(et-st))
-    #
-    print('# Z3计算中...')
-    st = time.clock()
-    if s.check() == sat:
+    with Solver() as s:
+        # 生成Frame约束
+        print('\t# 生成frame constraints...')
+        st = time.clock()
+        frame_constraints(s, testSet.frameSet)
         et = time.clock()
-        print('  耗时：{} s'.format(et-st))
-        print('# 已求解，一个可行解为：')
-        # resultFile = open('result/result_{}.txt'.format(int(time.time())), "w")
-        model = s.model()
-        print(model)
-        # resultFile.write(model)
+        print('\t  耗时：{} s'.format(et-st))
+        #
+        print('\t# 生成link constraints...')
+        st = time.clock()
+        link_constraints(s, testSet.frameSetSortByLink)
+        et = time.clock()
+        print('\t  耗时：{} s'.format(et-st))
+        # 同步精度通常为1us
+        print('\t# 生成virtual link constraints...')
+        st = time.clock()
+        virtual_link_constraints(s, testSet.frameSet, testSet.vlinkSet, 1)
+        et = time.clock()
+        print('\t  耗时：{} s'.format(et-st))
+        #
+        print('\t# 生成end to end latency constraints...')
+        st = time.clock()
+        end_to_end_latency_constraints(s, testSet.frameSet, testSet.vlinkSet)
+        et = time.clock()
+        print('\t  耗时：{} s'.format(et-st))
+        #
+        print('\t# 生成task constraints...')
+        st = time.clock()
+        task_constraints(s, testSet.frameSet, testSet.vlinkSet)
+        et = time.clock()
+        print('\t  耗时：{} s'.format(et-st))
+        #
+        print('\t# 生成virtual frame sequence constraints...')
+        st = time.clock()
+        virtual_frame_sequence_constraints(
+            s, testSet.frameSet, testSet.vlinkSet)
+        et = time.clock()
+        print('\t  耗时：{} s'.format(et-st))
+        #
+        print('# Z3计算中...')
+        st = time.clock()
+        if s.solver():
+            et = time.clock()
+            print('  耗时：{} s'.format(et-st))
+            print('# 已求解，一个可行解为：')
+            # resultFile = open('result/result_{}.txt'.format(int(time.time())), "w")
+            model = s.get_model()
+            print(model)
+            # resultFile.write(model)
 
-    else:
-        et = time.clock()
-        print('  耗时：{} s'.format(et-st))
-        print('# 没有可行解...')
-    # pdb.set_trace()
+        else:
+            et = time.clock()
+            print('  耗时：{} s'.format(et-st))
+            print('# 没有可行解...')
+        # pdb.set_trace()
 
     return True
